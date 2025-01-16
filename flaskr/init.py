2b14cafcedd702a0101df50422037b8c31c4077a
emustard1 <edward-mustard@hotmail.com>
2025-01-16 14:36:41 +0000

    Trying a new way to use forms

--- flaskr/init.py
+++ flaskr/init.py
@@ -1,31 +1,29 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
+from 
 
 app = Flask(__name__)
 
 maths = [('What is 1 + 1?', '2')]
-
+invalid = True
 
 @app.route('/')
 def home():
     return render_template('index.html')
   
 @app.route('/test', methods =["GET", "POST"])
-def question():
+def question(maths=maths):    
     
-    #get answer
+    #still need to fix this bit
+    user_answer = None
     if request.method == "POST":
-        user_answer = request.form.get("u_answer")
-      
-    #compare answer to original
-    if user_answer == maths[0][1]:
-        return render_template("correct.html")
-    elif user_answer != maths[0][1]:
-       return render_template("incorrect.html", user_answer)
+      user_answer = request.form.get('user_answer')
 
-      #we get input in a string
-      #need to compare this to the correct answer
-      #if 
-    return render_template('test.html')
+    if user_answer == maths[0][1]:
+       return redirect(url_for('correct'))
+
+
+    #original question template
+    return render_template('test.html', form='question_response')
 
 @app.route('/score')
 def display_scores():
